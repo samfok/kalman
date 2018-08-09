647330fdefffbaf64804c254e3478f7b22281cd1
samfok <samfok@stanford.edu>
2018-08-09 16:09:17 -0700

    bd script with ref direct and ref spiking

--- bd_kalman_1d.py
+++ bd_kalman_1d.py
@@ -5,7 +5,7 @@ import nengo_brainstorm
 from kalman import KalmanNet, LDSNet
 
 SYS_TAU = 0.01
-SIM_TIME = 10*SYS_TAU
+SIM_TIME = 50*SYS_TAU
 
 DELTA_T = 0.001 # discretization time
 A = np.array([[-1/SYS_TAU]])
@@ -13,53 +13,75 @@ B = np.array([[1/SYS_TAU]])
 C = np.array([[1]])
 D = np.zeros_like(A)
 Q = np.array([[0.1]])
-R = np.array([[0.01]])
+R = np.array([[0.001]])
 
-fig, ax = plt.subplots(figsize=(8, 6))
+STIM_PARAMS = {
+    0: 1,
+    10*SYS_TAU: 0,
+    20*SYS_TAU: 1,
+    30*SYS_TAU: 0,
+    40*SYS_TAU: 1,
+}
+
+fig, ax = plt.subplots(figsize=(16, 12))
 # run underlying dynamical system beforehand
-STIM_PARAMS = {0: 1, 5*SYS_TAU: 0}
-def run_underlying_system()
-    state_model = nengo.Network()
-    with state_model:
-        lds_net = LDSNet(A, B, C, D, Q, R)
-        stim = nengo.Node(nengo.utils.functions.piecewise(STIM_PARAMS))
-        nengo.Connection(stim, lds_net.input, synapse=None)
-        stim_probe = nengo.Probe(stim)
-        lds_state_probe = nengo.Probe(lds_net.state)
-        lds_out_probe = nengo.Probe(lds_net.output)
-    
-    sim = nengo.Simulator(state_model)
-    sim.run(SIM_TIME)
-    # set up run with braindrop,
-    trange = sim.trange().reshape(-1, 1)
-    stim_data = dict(np.hstack((trange, sim.data[stim_probe])))
-    measure_data = dict(np.hstack((trange, sim.data[lds_out_probe])))
-    return stim_data, measure_data
-STIM_DATA, MEASURE_DATA = run_underlying_system(A, B, C, D, Q, R, STIM_PARAMS)
+state_model = nengo.Network()
+with state_model:
+    lds_net = LDSNet(A, B, C, D, Q, R)
+    stim = nengo.Node(nengo.utils.functions.piecewise(STIM_PARAMS))
+    nengo.Connection(stim, lds_net.input, synapse=None)
+    stim_probe = nengo.Probe(stim)
+    lds_state_probe = nengo.Probe(lds_net.state)
+    lds_out_probe = nengo.Probe(lds_net.output)
+
+sim = nengo.Simulator(state_model)
+sim.run(SIM_TIME)
+# plot underlying dynamical system results
+ax.plot(sim.trange(), sim.data[stim_probe], alpha=0.5, label="stim")
+ax.plot(sim.trange(), sim.data[lds_state_probe], alpha=0.5, label="system state")
+ax.plot(sim.trange(), sim.data[lds_out_probe], alpha=0.3, label="measurement")
+
+# set up run with braindrop,
+trange = sim.trange().reshape(-1, 1)
+measure_data = dict(np.hstack((trange, sim.data[lds_out_probe])))
 
 # run with reference nengo
 ref_model = nengo.Network()
 with ref_model:
-    knet = KalmanNet(256, A, B, C, Q, R, DELTA_T)
-    stim_input = nengo.Node(nengo.utils.functions.piecewise(stim_data))
+    stim_input = nengo.Node(nengo.utils.functions.piecewise(STIM_PARAMS))
     stim_measure = nengo.Node(nengo.utils.functions.piecewise(measure_data))
-    nengo.Connection(stim_input, knet.input_system, synapse=None)
-    nengo.Connection(stim_measure, knet.input_system, synapse=None)
-    probe_readout = nengo.Probe(knet.readout)
+
+    knet_ref = KalmanNet(1, A, B, C, Q, R, DELTA_T, neuron_type=nengo.neurons.Direct())
+    knet_spiking = KalmanNet(256, A, B, C, Q, R, DELTA_T)
+
+    nengo.Connection(stim_input, knet_ref.input_system, synapse=None)
+    nengo.Connection(stim_input, knet_spiking.input_system, synapse=None)
+    nengo.Connection(stim_measure, knet_ref.input_measurement, synapse=None)
+    nengo.Connection(stim_measure, knet_spiking.input_measurement, synapse=None)
+
+    probe_readout_ref = nengo.Probe(knet_ref.readout)
+    probe_readout_spiking = nengo.Probe(knet_spiking.readout)
+
 sim = nengo.Simulator(ref_model)
 sim.run(SIM_TIME)
-sim.data[probe_readout]
 
-# run with braindrop
-model = nengo.Network()
-with model:
-    knet = KalmanNet(256, A, B, C, Q, R, DELTA_T)
-    stim_input = nengo.Node(nengo.utils.functions.piecewise(stim_data))
-    stim_measure = nengo.Node(nengo.utils.functions.piecewise(measure_data))
-    nengo.Connection(stim_input, knet.input_system, synapse=None)
-    nengo.Connection(stim_measure, knet.input_system, synapse=None)
-    probe_readout = nengo.Probe(knet.readout)
+# plot underlying dynamical system results
+ax.plot(sim.trange(), sim.data[probe_readout_ref], alpha=0.5, label="KF reference")
+ax.plot(sim.trange(), sim.data[probe_readout_spiking], alpha=0.5,
+        label="KF {} simulated nengo neurons".format(knet_spiking.state.n_neurons))
 
-sim = nengo_brainstorm.Simulator(model)
-sim.run(SIM_TIME)
+# # run with braindrop
+# model = nengo.Network()
+# with model:
+#     knet = KalmanNet(256, A, B, C, Q, R, DELTA_T)
+#     stim_input = nengo.Node(nengo.utils.functions.piecewise(STIM_PARAMS))
+#     stim_measure = nengo.Node(nengo.utils.functions.piecewise(measure_data))
+#     nengo.Connection(stim_input, knet.input_system, synapse=None)
+#     nengo.Connection(stim_measure, knet.input_system, synapse=None)
+#     probe_readout = nengo.Probe(knet.readout)
+# 
+# sim = nengo_brainstorm.Simulator(model)
+# sim.run(SIM_TIME)
 
+ax.legend(loc="best")
+plt.show()
